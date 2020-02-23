53cc669216216f07b9feaf0ee38dff84e28a1167
LimHyunJune <48524793+LimHyunJune@users.noreply.github.com>
2020-02-23 12:48:46 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
+REPOSITORY=/graduateProject # TODO 경로 수정
+cd $REPOSITORY
 
+echo "> 프로젝트 Build 시작"
+./gradlew build
+echo "> Build 파일 복사"
+cp ./build/libs/*.jar $REPOSITORY/
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -f springrest) # TODO 피드명 수정
+CURRENT_PID=$(pgrep -f graduateProject) # TODO 피드명 수정
 echo "$CURRENT_PID"
 if [ -z $CURRENT_PID ]; then
 
@@ -10,5 +16,8 @@ if [ -z $CURRENT_PID ]; then
                                 kill -9 $CURRENT_PID
                                         sleep 5
                                 fi
-                                echo "> 끝"
+                                echo "> 새 어플리케이션 배포"
+                                JAR_NAME=$(ls $REPOSITORY/ |grep 'graduateProject-0.0.1-SNAPSHOT.jar' | tail -n 1)
+                                echo "> JAR Name: $JAR_NAME"
+                                nohup java -jar $REPOSITORY/$JAR_NAME &
                                 
