2b3ba2604d206292fc7670636d2792cb0d18e06d
LimHyunJune <48524793+LimHyunJune@users.noreply.github.com>
2020-02-23 10:41:32 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -f graduateProject) # TODO 피드명 수정
+CURRENT_PID=$(pgrep -f springrest) # TODO 피드명 수정
 echo "$CURRENT_PID"
 if [ -z $CURRENT_PID ]; then
 
@@ -10,4 +9,6 @@ if [ -z $CURRENT_PID ]; then
                         echo "> kill -2 $CURRENT_PID"
                                 kill -9 $CURRENT_PID
                                         sleep 5
-                               
+                                fi
+                                echo "> 끝"
+                                
