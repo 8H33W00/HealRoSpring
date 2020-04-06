07351922d6c6c9661a57373b77d3b17cad520d3c
bbbyung2 <bhko0524@naver.com>
2020-04-06 14:58:13 +0900

    hospital

---
+++ graduateProject/src/main/java/com/health/entity/Hospital.java
@@ -0,0 +1,19 @@
+package com.health.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Hospital {
+	@Id
+	String hospitalCity;
+	String hospitalImg;
+	String hospitalName;
+	String hospitalAddr;
+	String hospitalWeb;
+	String hospitalPhone;
+	
+}
