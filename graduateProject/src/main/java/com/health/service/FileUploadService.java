b5a90616a93cccbe4bd7b0746506213d9ffa793d
LimHyunJune <baj2121@naver.com>
2020-04-10 23:05:08 +0900

    file test2

--- graduateProject/src/main/java/com/health/service/FileUploadService.java
+++ graduateProject/src/main/java/com/health/service/FileUploadService.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileUploadService {
 
 	
-	private static final String SAVE_PATH = System.getProperty("user.dir")+"/files/img";
+	private static final String SAVE_PATH = System.getProperty("user.dir")+"/src/main/resources/templates/files/img";
 	public void restore(MultipartFile multipartFile) {
 		
 		try {
