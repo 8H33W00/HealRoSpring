88e7d4eb3b4956b6a8023e928839c1b54285e4de
LimHyunJune <baj2121@naver.com>
2020-04-15 22:20:59 +0900

    맵 API 추가

---
+++ graduateProject/src/main/java/com/health/controller/MapController.java
@@ -0,0 +1,19 @@
+package com.health.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MapController {
+	
+	@GetMapping("/map")
+	public ModelAndView map() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("map");
+		
+		return mv;
+	}
+}
