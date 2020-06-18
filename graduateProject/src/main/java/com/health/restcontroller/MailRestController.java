6dc7d1a949ddd9e7a9531092be03ab020a4e36ae
USER <USER@LAPTOP-IL72SE3F>
2020-06-18 16:57:30 +0900

    mail 인코딩 해결

--- graduateProject/src/main/java/com/health/restcontroller/MailRestController.java
+++ graduateProject/src/main/java/com/health/restcontroller/MailRestController.java
@@ -1,11 +1,18 @@
 package com.health.restcontroller;
 
+import java.io.UnsupportedEncodingException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMessage.RecipientType;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -27,6 +34,9 @@ public class MailRestController {
 	@Autowired
 	MailService mailService;
 
+	@Autowired
+	JavaMailSender jmsender;
+	
 	@GetMapping("/mail")
 	public String sendMail( HttpSession session, HttpServletResponse response) 
 	{ 
@@ -34,12 +44,21 @@ public class MailRestController {
 		User user = userRepo.findByUserName(nickname); 
 		String m = mailService.coronaryContent(user) +"\n" + mailService.diabeteContent(user) +"\n" + mailService.cadioContent(user);
 				
-		SimpleMailMessage msg = new SimpleMailMessage(); 
+		
+		MimeMessage msg = jmsender.createMimeMessage();
+		try {
 	    msg.setFrom("HealRo@noreply"); 
-	    msg.setTo(user.getUserEmail()); 
-	    msg.setSubject("HealRo¿¡¼­ º¸³À´Ï´Ù."); 
-	    msg.setText(m); 
-	    this.sender.send(msg); 
+	    msg.addRecipient(RecipientType.TO, new InternetAddress(user.getUserEmail(), user.getUserName() +"´Ô", "utf-8"));
+
+	    msg.setSubject("HealRo¿¡¼­ º¸³À´Ï´Ù.","utf-8"); 
+	    msg.setText(m,"utf-8"); 
+	    jmsender.send(msg); 
+		}
+		catch (MessagingException e) {
+	        e.printStackTrace();
+	    } catch (UnsupportedEncodingException e) {
+	        e.printStackTrace();
+	    }
 	    return "";
 	    
 	}
